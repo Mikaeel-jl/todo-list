731af5c2d124d4296d0dd86e0004ed8adb434c6b
Mikaeel <mikaeel921@gmail.com>
2022-07-23 04:51:09 +0430

    add alert for delete btn , empty input after add task

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const todoInput = document.querySelector(".todo-input");
 const addBtn = document.querySelector(".add-btn");
-// const deleteBtn = document.querySelector(".fa-solid .fa-trash-can");
-// const doneBtn = document.querySelector(".fa-solid .fa-square-check");
 const todoContent = document.querySelector(".todo-content");
-// const todoTask = document.querySelector(".todo-task");
 
 function addTodo() {
   let todo = document.createElement("div");
@@ -20,11 +17,13 @@ function addTodo() {
   </div>
   `;
   todoContent.appendChild(todo);
-
+  todoInput.value = "";
   todo.addEventListener("click", (b) => {
     // console.log(b.target.classList.contains("fa-trash-can"));
     if (b.target.classList.contains("fa-trash-can")) {
-      todo.remove();
+
+      (confirm("do you want to delete the task ?") && todo.remove())
+      
     } else if (b.target.classList.contains("fa-square-check")) {
       todo.classList.toggle("todo-task-checked");
     }
