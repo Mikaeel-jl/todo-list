7407d39cc3e7b4aa91eca6084386df9e0ae12742
Mikaeel <mikaeel921@gmail.com>
2022-07-25 21:14:34 +0430

    add localStorage

--- script.js
+++ script.js
@@ -20,21 +20,66 @@ function addTodo(e) {
   `;
   todoContent.appendChild(todoDiv);
 
+  setLocal(todoInput.value);
+
   todoInput.value = "";
 }
 
 function deleteCheckBtn(e) {
   const classList = [...e.target.classList];
 
+  const todoDiv = e.target.parentElement.parentElement;
+  console.log(todoDiv.querySelector("p").innerText);
   if (classList[1] === "fa-square-check") {
-    e.target.parentElement.parentElement.classList.toggle("todo-task-checked");
+    todoDiv.classList.toggle("todo-task-checked");
   } else if (classList[1] === "fa-trash-can") {
-    confirm("do you want to delete the task ?") &&
-      e.target.parentElement.parentElement.remove();
+    deleteLocal(todoDiv.querySelector("p").innerText);
+    todoDiv.remove();
   }
 }
 
+function setLocal(value) {
+  let localTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+
+  localTodos.push(value);
+
+  localStorage.setItem("todos", JSON.stringify(localTodos));
+}
+
+function getLocal() {
+  let localTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+
+  localTodos.forEach((todo) => {
+    let todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo-task");
+    todoDiv.innerHTML = `
+    <p>
+      ${todo}
+    </p>
+    <div>
+      <i class="fa-solid fa-trash-can"> </i>
+      <i class="fa-solid fa-square-check"> </i>
+    </div>
+  `;
+    todoContent.appendChild(todoDiv);
+  });
+}
+
+function deleteLocal(todo) {
+  let localTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+
+  const filteredTodos = localTodos.filter((item) => item !== todo);
+
+  localStorage.setItem("todos", JSON.stringify(filteredTodos));
+}
 
 // event listners
 addBtn.addEventListener("click", addTodo);
 todoContent.addEventListener("click", deleteCheckBtn);
+document.addEventListener("DOMContentLoaded", getLocal);
